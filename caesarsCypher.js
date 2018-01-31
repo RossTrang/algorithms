6d378f059f74cfc776a8b942587918ed674f8bb9
Ross <rossrtw2010@gmail.com>
2018-01-31 23:52:15 +0700

    Last of the basic algorithm section

---
+++ caesarsCypher.js
@@ -0,0 +1,22 @@
+/*  https://www.freecodecamp.org/challenges/caesars-cipher
+- str.charCodeAt(i) need to iterate through characters of string // str.length or convert to array?
+- write the returned .fromCharCode character to an object
+- alphanumerics need to shift 13 points A=N. non-alphanumerics should not be transformed
+- UTF-16 values? A=65, B=66...M=77, N=78 need logic to wrap 2nd half of alphabet N=A (78=65)
+*/
+function rot13(str) { // LBH QVQ VG!
+  
+  var decoded = "";
+      
+  for(var character in str) { 
+    charCode = str.charCodeAt(character);       //console.log(charCode);
+        if (charCode >=65 && charCode <=77) {charCode = charCode + 13;}   
+          else if (charCode >=78 && charCode <=90) {charCode = charCode - 13;}   //console.log(charCode);
+    decoded = (decoded + String.fromCharCode(charCode));     //console.log(decoded);
+  }
+    //console.log(decoded);
+  return decoded;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
